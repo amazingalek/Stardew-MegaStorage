75e407bd35ab01c03bee4b81ff13fa5923bc57e9
Aleksander Waage <alek.ntnu@gmail.com>
2019-06-21 21:46:57 +0200

    cleanup

--- MegaStorage/Models/ModConfig.cs
+++ MegaStorage/Models/ModConfig.cs
@@ -12,16 +12,4 @@
 
         public static ModConfig Instance { get; private set; }
     }
-
-    public class CustomChestConfig
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Recipe { get; set; }
-        public string SpritePath { get; set; }
-        public string SpriteBWPath { get; set; }
-        public string SpriteBracesPath { get; set; }
-    }
-
 }
