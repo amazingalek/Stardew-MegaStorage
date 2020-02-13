caebf0eb2b02aef36405224230d07d61ab1bca3f
Matthew <justmatt@outlook.com>
2020-02-13 00:05:13 -0600

    Using static Helper/Monitor

--- MegaStorage/MenuChanger.cs
+++ MegaStorage/MenuChanger.cs
@@ -9,23 +9,14 @@ namespace MegaStorage
 {
     public class MenuChanger
     {
-        private readonly IModHelper _modHelper;
-        private readonly IMonitor _monitor;
-
-        public MenuChanger(IModHelper modHelper, IMonitor monitor)
-        {
-            _modHelper = modHelper;
-            _monitor = monitor;
-        }
-
         public void Start()
         {
-            _modHelper.Events.Display.MenuChanged += OnMenuChanged;
+            MegaStorageMod.ModHelper.Events.Display.MenuChanged += OnMenuChanged;
         }
 
         private void OnMenuChanged(object sender, MenuChangedEventArgs e)
         {
-            _monitor.VerboseLog("New menu: " + e.NewMenu?.GetType());
+            MegaStorageMod.ModMonitor.VerboseLog("New menu: " + e.NewMenu?.GetType());
             if (e.NewMenu is LargeItemGrabMenu)
                 return;
             if (!(e.NewMenu is ItemGrabMenu itemGrabMenu) || !(itemGrabMenu.context is CustomChest customChest))
