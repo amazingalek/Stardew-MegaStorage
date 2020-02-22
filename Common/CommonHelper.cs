a62093014ddd6932abacdd24a883433ec576ac8c
Matthew <justmatt@outlook.com>
2020-02-22 12:33:06 -0600

    Added Shared Project for Helper Functions

---
+++ Common/CommonHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StardewValley;
+using StardewValley.Locations;
+
+namespace furyx639.Common
+{
+    internal static class CommonHelper
+    {
+        /*********
+        ** Public methods
+        *********/
+        public static IEnumerable<GameLocation> GetLocations()
+        {
+            return Game1.locations
+                .Concat(
+                    from location in Game1.locations.OfType<BuildableGameLocation>()
+                    from building in location.buildings
+                    where building.indoors.Value != null
+                    select building.indoors.Value
+                );
+        }
+
+        public static T NonNull<T>(T obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return obj;
+        }
+    }
+}
