762537f534d91eb19e47509af13ed17458a074e4
Matthew <justmatt@outlook.com>
2020-02-29 15:26:50 -0600

    Simplified Mapping Extensions

--- MegaStorage/Framework/MappingExtensions.cs
+++ MegaStorage/Framework/MappingExtensions.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Linq;
-using MegaStorage.Framework.Models;
+﻿using MegaStorage.Framework.Models;
 using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewValley.Objects;
+using System;
+using System.Linq;
 using SObject = StardewValley.Object;
 
 namespace MegaStorage.Framework
@@ -28,47 +28,34 @@ namespace MegaStorage.Framework
             var chest = new Chest(customChest.playerChest.Value, customChest.TileLocation)
             {
                 name = customChest.name,
-                Stack = customChest.Stack
+                Stack = customChest.Stack,
+                ParentSheetIndex = customChest.ParentSheetIndex
             };
 
             chest.items.AddRange(customChest.items);
             chest.playerChoiceColor.Value = customChest.playerChoiceColor.Value;
 
+            MegaStorageMod.ConvenientChests?.CopyChestData(customChest, chest);
+
             return chest;
         }
 
-        public static CustomChest ToCustomChest(this Item item) =>
-            item.ToCustomChest(Vector2.Zero);
-        public static CustomChest ToCustomChest(this Item item, Vector2 tileLocation) =>
-            item.ToCustomChest(
-                CustomChestFactory.CustomChests.FirstOrDefault(x => x.Value == item.ParentSheetIndex).Key,
-                tileLocation);
-        public static CustomChest ToCustomChest(this Item item, ChestType chestType) =>
-            item.ToCustomChest(chestType, Vector2.Zero);
-        public static CustomChest ToCustomChest(this Item item, ChestType chestType, Vector2 tileLocation)
+        public static CustomChest ToCustomChest(this Item item, Vector2? tileLocation = null) =>
+            item.ToCustomChest(CustomChestFactory.CustomChests.FirstOrDefault(x => x.Value == item.ParentSheetIndex).Key, tileLocation ?? Vector2.Zero);
+        public static CustomChest ToCustomChest(this Item item, ChestType chestType, Vector2? tileLocation = null)
         {
             if (!(item is Chest chest))
                 throw new InvalidOperationException($"Cannot convert {item?.Name} to CustomChest");
 
-            var customChest = CustomChestFactory.Create(chestType, tileLocation);
+            var customChest = CustomChestFactory.Create(chestType, tileLocation ?? chest.TileLocation);
             customChest.name = chest.name;
             customChest.Stack = chest.Stack;
             customChest.items.AddRange(chest.items);
             customChest.playerChoiceColor.Value = chest.playerChoiceColor.Value;
 
+            MegaStorageMod.ConvenientChests?.CopyChestData(chest, customChest);
+
             return customChest;
         }
-
-        public static DeserializedChest ToDeserializedChest(this Item item, string locationName, Vector2 position) =>
-            item is CustomChest customChest
-                ? new DeserializedChest
-                {
-                    LocationName = locationName,
-                    PositionX = position.X,
-                    PositionY = position.Y,
-                    ChestType = customChest.ChestType,
-                    Name = customChest.name
-                }
-                : throw new InvalidOperationException($"Cannot convert {item?.Name} to DeserializedChest");
     }
 }
